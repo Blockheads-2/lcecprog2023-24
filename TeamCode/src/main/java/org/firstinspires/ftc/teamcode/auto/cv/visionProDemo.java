480ea5573878446ab31203f06870832ee98f44af
Josh Sugino <jsugino25@gmail.com>
2023-10-04 16:33:35 -0700

    got rid of the weird million js files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/cv/visionProDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/cv/visionProDemo.java
@@ -5,6 +5,7 @@ import android.util.Size;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
